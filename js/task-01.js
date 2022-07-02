c25b1a1c5a1adbb20e3037fb946119e93cbda961
Yurii Kavuza <kavuzaiurii@gmail.com>
2022-07-02 13:42:50 +0300

    Added favicon

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categories = document.querySelectorAll("#categories .item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach((element) => {
+    return console.log(`Category: ${element.querySelector("h2").textContent}
+Elements:${element.querySelectorAll("li").length}`);
+});
