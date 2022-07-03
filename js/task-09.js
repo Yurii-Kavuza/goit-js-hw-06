8a6273efc5fc23489d9c227cbaa26190a7288a0c
Yurii Kavuza <kavuzaiurii@gmail.com>
2022-07-03 11:06:47 +0300

    task9 solved

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnRef = document.querySelector(".change-color");
+const changeColorTextRef = document.querySelector(".color");
+const changeBGRef = document.querySelector("body");
+
+
+changeColorBtnRef.addEventListener("click", event => {
+  const currentColor = getRandomHexColor();
+  changeBGRef.style.backgroundColor = currentColor;
+  changeColorTextRef.textContent = currentColor;
+});
